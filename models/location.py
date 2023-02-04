84bda6bca7a7cd97048bdfd3d7a44fa6cc61ae3f
gramscott <120787031+gramscott@users.noreply.github.com>
2023-02-04 17:02:42 +0000

    Models made

---
+++ models/location.py
@@ -0,0 +1,12 @@
+class Location:
+    
+    def __init__(self, name, set, filmed = False, id = None):
+        self.name = name 
+        self.set = set 
+        self.filmed = filmed 
+        self.id = id 
+        
+        
+        
+    def mark_filmed(self):
+        self.filmed = True
--- models/user.py
+++ models/user.py
@@ -1,2 +1,11 @@
+class User:
+    
+    def __init__(self, name, job, id=None):
+        self.name = name 
+        self.job = job 
+        self.id = id
 
-        
+
+
+
+
