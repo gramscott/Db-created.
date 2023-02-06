869a773c158730be2f3ec16e1fbb78682cbdf5b8
gramscott <120787031+gramscott@users.noreply.github.com>
2023-02-06 09:36:42 +0000

    user_repository tested

--- console.py
+++ console.py
@@ -5,7 +5,7 @@ import repositories.location_repository as location_repository
 import repositories.user_repository as user_repository 
 
 # user_repository.delete_all()
-# user_repository.select_all()
+user_repository.select_all()
 
 user1 = User("Susan", "Tv Producer")
 user_repository.save(user1)
@@ -13,10 +13,10 @@ user2 = User("Greg", "Assistant Director")
 user_repository.save(user2)
 
 
-location1 = Location("Glasgow", "River City", True, False, user1.id)
-location_repository.save(location1)
-location2 = Location("Edinburgh", "Avengers: Infinity War", False, False, user2.id)
-location_repository.save(location2)
+# location1 = Location("Glasgow", "River City", True, False, user1.id)
+# location_repository.save(location1)
+# location2 = Location("Edinburgh", "Avengers: Infinity War", False, False, user2.id)
+# location_repository.save(location2)
 
 # location_repository.delete_all()
 # location_repository.select_all()
--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -1,10 +1,32 @@
-from flask import Flask,  render_template
+from flask import Flask, render_template, request, redirect
 
 from flask import Blueprint
 
 users_blueprint = Blueprint("users", __name__)
 
+import repositories.user_repository as user_repository
+
+# import repositories.location_repository as location_repository
 
 @users_blueprint.route("/users")
-def locations():
-    return render_template("users/index.html")
+def users():
+    users = user_repository.select_all()
+    return render_template ("users/index.html", users = users)
+
+# NEW
+# GET '/tasks/new'
+@users_blueprint.route("/users/new", method =['GET'])
+# CREATE
+# POST '/tasks'
+
+# SHOW
+# GET '/tasks/<id>'
+
+# EDIT
+# GET '/tasks/<id>/edit'
+
+# UPDATE
+# PUT '/tasks/<id>'
+
+# DELETE
+# DELETE '/tasks/<id>'
